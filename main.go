11832496bec868dbf1eee7e5366ba75a89c6688d
Dmitrii Makarov <megabrain87@gmail.com>
2025-05-16 20:42:46 +0200

    feat(app): create basic server

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
-func main()	{
+import (
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/DmitryMA/go-Chi-PostgreSQL-Docker/internal/app"
+)
+
+func main()	{
+	application, err := app.NewApplication()
+
+	if err != nil {
+		panic(err)
+	}
+
+	application.Logger.Println("We are running our App")
+
+	http.HandleFunc("/health", HealthCheck)
+
+	server := &http.Server{
+		Addr: ":8080",
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+
+	if err != nil {
+		application.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
 }
